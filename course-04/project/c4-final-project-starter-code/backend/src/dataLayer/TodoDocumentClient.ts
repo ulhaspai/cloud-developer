65a4e4af865824079d4eabfa8997d24ad8ca8975
Ulhas Pai <upai@kayak.com>
2020-04-26 03:06:13 -0400

    - added update todo

--- course-04/project/c4-final-project-starter-code/backend/src/dataLayer/TodoDocumentClient.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/dataLayer/TodoDocumentClient.ts
@@ -2,6 +2,9 @@ import { DocumentClient } from "aws-sdk/lib/dynamodb/document_client"
 import * as AWS from "aws-sdk"
 import { TodoItem } from "../models/TodoItem"
 import { ITodoDataAccess } from "./ITodoDataAccess";
+import { createLogger } from "../utils/logger";
+
+const logger = createLogger('createTodo')
 
 /**
  * TodoDocumentClient is an AWS implementation for the {@link ITodoDataAccess}
@@ -17,7 +20,7 @@ export class TodoDocumentClient implements ITodoDataAccess {
 
     private static getDynamoDBClient(): DocumentClient {
         if (process.env.IS_OFFLINE) {
-            console.log('Creating a local DynamoDB instance')
+            logger.info('Creating a local DynamoDB instance')
             return new AWS.DynamoDB.DocumentClient({
                 region: 'localhost',
                 endpoint: 'http://localhost:8000'
@@ -37,27 +40,50 @@ export class TodoDocumentClient implements ITodoDataAccess {
             }
         }).promise()
 
-        return result.Items as Array<TodoItem>
+        return result.Count > 0
+            ? result.Items.map(i => TodoDocumentClient.convertDbItemToTodoItem(i))
+            : []
+    }
+
+    async getTodo(todoId: string, userId: string): Promise<TodoItem> {
+        const result = await this.documentClient.query({
+            TableName: TodoDocumentClient.TODOS_TABLE,
+            IndexName: TodoDocumentClient.USER_ID_INDEX,
+            KeyConditionExpression: 'todoId = :todoId and userId = :userId',
+            ExpressionAttributeValues: {
+                ':todoId': todoId,
+                ':userId': userId
+            }
+        }).promise()
+
+        if (result.Count != 0) {
+            return Promise.resolve(TodoDocumentClient.convertDbItemToTodoItem(result.Items[0]))
+        }
+        return Promise.resolve(null)
     }
 
     async createTodo(todoItem: TodoItem): Promise<TodoItem> {
         try {
-            console.log("TodoDocumentClient: creating new todo = ", todoItem)
             await this.documentClient.put({
                 TableName: TodoDocumentClient.TODOS_TABLE,
                 Item: todoItem
             }).promise()
             return Promise.resolve(todoItem)
         } catch (err) {
-            console.log(" Error creating todo item", err)
+            logger.info(" Error creating todo item", err)
         }
+    }
+
+    async updateTodo(todoItem: TodoItem): Promise<TodoItem> {
+        await this.documentClient.put({
+            TableName: TodoDocumentClient.TODOS_TABLE,
+            Item: todoItem,
+        }).promise()
+        return Promise.resolve(todoItem);
     }
 
     async deleteTodo(todoId: string, userId: string): Promise<string> {
         try {
-            console.log("TodoDocumentClient: deleting todo todoId = ", todoId)
-            console.log("TodoDocumentClient: deleting todo userId = ", userId)
-
             await this.documentClient.delete({
                 TableName: TodoDocumentClient.TODOS_TABLE,
                 Key: {
@@ -67,8 +93,20 @@ export class TodoDocumentClient implements ITodoDataAccess {
             }).promise()
             return Promise.resolve(todoId)
         } catch (err) {
-            console.log("Error deleting todo item", err)
+            logger.info("Error deleting todo item", err)
         }
         throw new Error("Method not implemented.");
     }
+
+    private static convertDbItemToTodoItem(dbItem:  DocumentClient.AttributeMap): TodoItem {
+        return {
+            todoId: dbItem.todoId,
+            userId: dbItem.userId,
+            createdAt: dbItem.createdAt,
+            name: dbItem.name,
+            dueDate: dbItem.dueDate,
+            done: dbItem.done,
+        }
+    }
+
 }
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -1,14 +1,36 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { createLogger } from "../../utils/logger";
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { LambdaUtils } from "../LambdaUtils";
+import { TodoManager } from "../../businessLogic/TodoManager";
 
-// import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+const logger = createLogger('updateTodo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  event
-  // const todoId = event.pathParameters.todoId
-  // const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+    logger.info("updateTodo: Started")
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+    // grab the user id from the JWT payload
+    const userId: string = LambdaUtils.getUserId(event)
+    logger.info("updateTodo: userId = ", userId)
+
+    // grab the todoId to be deleted
+    const todoId = event.pathParameters.todoId
+    logger.info("updateTodo: todoId = ", todoId)
+
+    // grab the request body
+    const updateTodoRequest: UpdateTodoRequest = JSON.parse(event.body)
+
+    // update the to-do item for this user
+    await TodoManager.updateTodoItem(todoId, userId, updateTodoRequest)
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        },
+        body: null
+    }
 }
